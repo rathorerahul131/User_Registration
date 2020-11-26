a9066f6713b01b0f5536fab7ababf46cace86711
neelrahulr <rathorerahul131@gmail.com>
2020-11-26 09:01:24 +0530

    Refactored: Validated First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,14 @@
 #!/bin/bash/
 
 echo "Welcome to The User Registration System"
+
+read -p "Enter your First Name keeping first letter as capital: " firstName
+
+reg_pattern='^[A-Z]{1}[a-z]{2,}$'
+
+if [[ $firstName =~ $reg_pattern ]]
+then
+       echo "Valid Entry : $firstName "
+else
+       echo "Invalid Entry! Please ensure that first letter of your name is capital"
+fi
